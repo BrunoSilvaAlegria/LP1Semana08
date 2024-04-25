a292bf89639b75998bbe2aa0d75671a0b4bb67d9
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-04-25 16:02:21 +0100

    Ex 2, initialize property from ICanFly interface and returns the respective numbers

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
-    public class Bat : Animal, IMammal
+    public class Bat : Animal, IMammal, ICanFly
     {
         //Added bat sound with the same method from Dog and Cat
         public override string Sound()
@@ -15,5 +15,8 @@ namespace AnimalKingdom
         
         //Returns the asked number using the property from the IMammal interface
         public int NumberOfNipples { get => 2; }
+        
+        //Returns the asked number using the property from the ICanFly interface
+        public int NumberOfWings { get => 2; }
     }
 }
--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
-    public class Bee : Animal
-    {
+    public class Bee : Animal, ICanFly
+    {   
+        //Added bee sound with the same method from Dog and Cat
         public override string Sound()
         {
             return base.Sound() + "Bzzzz";
         }
+        //Returns the asked number using the property from the ICanFly interface
+        public int NumberOfWings { get => 2; }
     }
 }
